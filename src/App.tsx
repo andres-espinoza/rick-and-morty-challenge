ad48f2618e557655b27606505eaf6526525d1399
Andrés Espinoza D <andres.espinozad@mail.udp.cl>
2022-11-16 19:05:33 -0300

    Cleaning App of initial request

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,6 @@ import { useRoutes } from 'react-router-dom';
 import routes from './routes';
 
 const App = () => {
-  // const dispatch = useAppDispatch();
-  // const { loading, error, charactersBasicData } = useAppSelector(
-  //   (state) => state.characters
-  // );
-
-  // const initCharacters = useCallback(async () => {
-  //   await dispatch(getCharactersBasicData());
-  // }, [dispatch]);
-
-  // useEffect(() => {
-  //   if (!charactersBasicData.length) {
-  //     initCharacters().catch((err) => console.log(err));
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
   const content = useRoutes(routes);
   return content;
 };
