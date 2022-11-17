745c70842221e4fe968a0bd68c33ee3c694b4714
Andrés Espinoza D <andres.espinozad@mail.udp.cl>
2022-11-17 20:08:36 -0300

    Home Updated

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,13 @@
-import { Typography, Box } from '@mui/material';
+import { Typography, Box, useMediaQuery } from '@mui/material';
 import RickAndMortyLogo from '../components/icons/RickAndMortyLogo';
 
 const Home = () => {
+  const xl = useMediaQuery('(min-width:1536px)');
+  const lg = useMediaQuery('(min-width:1200px)');
+  const md = useMediaQuery('(min-width:900px)');
+  const sm = useMediaQuery('(min-width:600px)');
+  // eslint-disable-next-line no-nested-ternary
+  const height = xl ? 250 : lg ? 200 : md ? 180 : sm ? 100 : 80;
   return (
     <Box
       width="100%"
@@ -10,6 +16,9 @@ const Home = () => {
         flexDirection: 'column',
         alignItems: 'center',
         gap: 5,
+        marginTop: {
+          xs: 3,
+        },
       }}
     >
       <div>
@@ -17,6 +26,9 @@ const Home = () => {
           variant="h2"
           width="100%"
           textAlign="center"
+          fontSize={{
+            xs: '1.2rem',
+          }}
         >
           Welcome to the
         </Typography>
@@ -24,14 +36,25 @@ const Home = () => {
           variant="h1"
           width="100%"
           textAlign="center"
+          fontSize={{
+            xs: '2rem',
+          }}
         >
           Rick & Morty App!
         </Typography>
       </div>
       <RickAndMortyLogo
-        height={100}
+        height={height}
         className="pulse"
       />
+      <Typography
+        variant="body1"
+        width="80%"
+        textAlign="center"
+        fontWeight={700}
+      >
+        Discover Characters & Find Episodes
+      </Typography>
     </Box>
   );
 };
